3f3ab9ade4d9c1b670ec4fd8e04f8be8e79e406a
NowshinNawar <68994761+Nowshin73@users.noreply.github.com>
2023-07-24 23:03:20 +0600

    college detail updated

--- bd-college-booking/src/pages/colleges/CollegeDetail.jsx
+++ bd-college-booking/src/pages/colleges/CollegeDetail.jsx
@@ -2,26 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams} from 'react-router-dom';
 
 const CollegeDetail = () => {
-    const id = useParams();
-    console.log(id);
-    const [college, setCollege] = useState([]);
-    
-  
-    useEffect(() => {
-        fetch(`http://localhost:5000/colleges`)
-            .then(response => response.json())
-            .then(data => setCollege(data))
-            .catch(error => console.error('Error fetching classes:', error));
-    }, []);
    
+    const college = useLoaderData();
+  
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">{college.collegename}</h1>
-      <div className="grid grid-cols-2 gap-4">
+      <h1 className="text-4xl font-bold mb-4">{college.collegename}</h1>
+      <div className="grid grid-cols-2 gap-20">
         <div>
           <img src={college.clgimage} alt={college.collegename} className="w-full rounded-lg" />
-          <h2 className="text-xl font-bold mt-4">Admission Process</h2>
-          <p className="mt-2">{college.admission_date}</p>
+          <div className='w-[500px] h-[300px]'>
+          <h2 className="text-2xl font-bold mt-4">Admission Process</h2>
+          <p className="my-2 font-semibold">Admission Date: {college.admission_date}</p>
+          <p className='font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam suscipit molestiae nostrum, repellat, ratione vel repudiandae tempora ab natus adipisci minima necessitatibus aliquam? </p>
+          </div>
         </div>
         <div>
           <h2 className="text-xl font-bold">Events Details</h2>
