0cbf19b6b0fccf70b15a3620615de70cfac9fd12
Nowshin Nawar <68994761+Nowshin73@users.noreply.github.com>
2024-05-21 13:20:42 +0600

    category added

--- bd-college-booking/src/pages/colleges/Colleges.jsx
+++ bd-college-booking/src/pages/colleges/Colleges.jsx
@@ -5,6 +5,7 @@ import Siderbar from './sidebar/Siderbar';
 
 const Colleges = () => {
   const [colleges, setColleges] = useState([]);
+  const [filteredcolleges, setFilteredColleges] = useState([]);
   //const [instructors, setInstructors] = useState([]);
 
   useEffect(() => {
@@ -13,15 +14,36 @@ const Colleges = () => {
       .then(data => setColleges(data))
       .catch(error => console.error('Error fetching classes:', error));
   }, []);
+  const Category = (college) => {
+    if (college == "all") {
+      setFilteredColleges(colleges);
+    }
+    if (college == "medical") {
+      const medical = colleges.filter(data => colleges.category === "Medical");
+      console.log(medical);
+      setFilteredColleges(colleges.filter(data => colleges.category === "Medical"));
+    }
+    if (college == "engg") {
+      const engg = colleges.filter(data => colleges.category === "Engineering");
+      setFilteredColleges(colleges.filter(data => colleges.category === "Engineering"));
+    }
+    if (college == "fasion") {
+      const fasion = colleges.filter(data => colleges.category === "Fasion & Design");
+      setFilteredColleges(colleges.filter(data => colleges.category === "Fasion & Design"));
+    }
+  }
   const allColleges = colleges.slice(0, 20);
   return (
     <div className="container  flex">
       <div className='sidebar flex flex-col w-[20%] bg-white shadow-md text-black font-semibold'>
-        <Siderbar colleges={colleges}></Siderbar>
+        <button className='p-5' onClick={() => Category("all")}>All Colleges</button>
+        <button className='p-5' onClick={() => Category("medical")}> Medical Colleges</button>
+        <button className='p-5' onClick={() => Category("engg")}> Engineering Colleges</button>
+        <button className='p-5' onClick={() => Category("fasion")}>Fasion & Design</button>
       </div>
 
       <div className='clg-container grid md:grid-cols-2 lg:grid-cols-3 justify-center'>
-        {allColleges.map((college) => (
+        {filteredcolleges.map((college) => (
           <College key={college.id} college={college} />
         ))}
       </div>
